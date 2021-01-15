ed5aadcd9629b05e76555445ebf8292b4fae3b14
l-schmidt <landarchbim@gmail.com>
2021-01-15 10:56:17 -0800

    creation of Topography class

---
+++ src/Landform/Topography.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+
+namespace Landform
+{
+    public class Topography
+    {
+        public static List<Point> GetPoints(Revit.Elements.Topography topography)
+        {
+            var points = topography.Points;
+            return points;
+        }
+    }
+}
