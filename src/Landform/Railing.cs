16ee5831f3a14bc13b9e487e5d7515f64fc1cf79
l-schmidt <landarchbim@gmail.com>
2021-01-22 11:14:04 -0800

    Update Railing.cs

--- src/Landform/Railing.cs
+++ src/Landform/Railing.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Analysis;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Dynamo.Graph.Nodes;
+using Revit.GeometryConversion;
+using RevitServices.Transactions;
 
 namespace Landform
 {
     public class Railing
     {
+        private Railing();
+        {
+        }
+
+        public static void ChangeHost(Element railing, Element host)
+        {
+            var id = railing.Id;
+
+        }
     }
 }
